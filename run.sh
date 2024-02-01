40e6fbd282181971acec5e808e97dae868b8e4be
Christoffer Lundström <lundstromc90@gmail.com>
2024-02-01 20:32:42 +0100

    ldap connection working

--- run.sh
+++ run.sh
@@ -4,10 +4,8 @@ docker build -t ldap ldap
 docker rm -f ldap || echo "No container to remove"
 
 docker run \
-	--network=host \
 	--name ldap \
 	-d \
-	-e LOG_LEVEL=debug \
 	-p 389:389 \
 	-p 636:636 \
 	ldap
