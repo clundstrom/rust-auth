6db89e3dcee3d3466a4c32f689b63977996a14d3
Christoffer Lundström <lundstromc90@gmail.com>
2024-02-01 20:26:58 +0100

    working config

--- run.sh
+++ run.sh
@@ -6,7 +6,11 @@ docker rm -f ldap || echo "No container to remove"
 docker run \
 	--network=host \
 	--name ldap \
+	-d \
+	-e LOG_LEVEL=debug \
 	-p 389:389 \
 	-p 636:636 \
-	-v //users.ldif:/home/ldif/users.ldif \
 	ldap
+
+sleep 5
+docker exec ldap ldapadd -x -D "cn=admin,dc=example,dc=com" -w password -f users.ldif
